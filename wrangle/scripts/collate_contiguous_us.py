37ee56b86302bea83efc4ef44b73981ee6511b2c
Dan Nguyen <dansonguyen@gmail.com>
2016-08-15 22:05:14 -0700

    ok

--- wrangle/scripts/collate_contiguous_us.py
+++ wrangle/scripts/collate_contiguous_us.py
@@ -1,11 +1,11 @@
 from settings import FETCHED_DIR as SRC_DIR
 from csv import DictReader, DictWriter
-from sys import stdout
+from sys import stdout, argv
 
 US_LATITUDES = [24.6, 50]
 US_LONGITUDES = [-125, -65]
 
-def main():
+def main(start_datestr="0000", end_datestr="9999"):
     filenames = list(SRC_DIR.glob('*.csv'))
     with filenames[0].open('r') as _f:
         headers = _f.readline().strip().split(',')
@@ -18,9 +18,15 @@ def main():
             for row in DictReader(rf):
                 lat = float(row['latitude'])
                 lng = float(row['longitude'])
+                date = row['time']
+
                 if lat >= US_LATITUDES[0] and lat <= US_LATITUDES[1]\
-                and lng >= US_LONGITUDES[0] and lng <= US_LONGITUDES[1]:
+                and lng >= US_LONGITUDES[0] and lng <= US_LONGITUDES[1]\
+                and date >= start_datestr and date < end_datestr:
                     outcsv.writerow(row)
 
 if __name__ == '__main__':
-    main()
+
+    x = argv[1] if len(argv) > 1 else '0000'
+    y = argv[2] if len(argv) > 2 else '9999'
+    main(x, y)
