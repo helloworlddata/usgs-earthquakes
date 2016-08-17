710ec422996874ce6c791ab39fccc5de40e04160
Dan Nguyen <dansonguyen@gmail.com>
2016-08-17 12:47:45 -0700

    no more settings

--- wrangle/scripts/settings.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-FETCHED_DIR = Path('wrangle', 'corral', 'fetched')
-FETCHED_DIR.mkdir(exist_ok=True, parents=True)
-COLLATED_DIR = Path('wrangle', 'corral', 'collated')
-COLLATED_DIR.mkdir(exist_ok=True, parents=True)
